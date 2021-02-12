be7f125fe62ebc350af8d4e07b28afa601dd8990
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-12 13:43:20 -0300

    Styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, world!</Text>
-    </View>
+    <>
+      <StatusBar  barStyle="light-content" backgroundColor="transparent" translucent/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello, world!</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 34,
+    fontWeight: 'bold',
+  }
+});
